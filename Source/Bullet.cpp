defefc9f2525bdef7c627789f116a52c6461cfbd
啓大 齋藤 <saitou-keita0109@outlook.jp>
2025-08-05 15:52:27 +0900

    ✨️ Bulletが一定時間後に消えるようになった

--- Source/Bullet.cpp
+++ Source/Bullet.cpp
@@ -6,6 +6,7 @@ using namespace mtgb;
 namespace
 {
 	const float BULLET_SPEED{ 10.f }; // 1秒当たりの移動ピクセルスピード
+	int timer{ 0 };
 }
 
 Bullet::Bullet(const Vector3& _position) : GameObject(GameObjectBuilder()
@@ -30,15 +31,13 @@ Bullet::~Bullet()
 
 void Bullet::Update()
 {
-	//static int timer = 0;
-	//timer += 1;
-	//if (InputUtil::GetKeyDown(KeyCode::V) /*|| timer > 60 * 5*/)
-	//{
-	//	DestoryMe();
-	//}
-
-	//pTransform_->position_.z += 0.1f;
 	//LOGF("bulletだよ imagehandle:%d\n", hImage_);
+	timer += 1;
+	if (timer >= 300)
+	{
+		timer = 0;
+		DestoryMe();
+	}
 	pRb_->velocity_ = { 0, 0, BULLET_SPEED };
 }
 
