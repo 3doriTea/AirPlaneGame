92f8135b45e044815cb3a428e2c9eb88e044c470
西川和樹 <190053668+nishi-0831@users.noreply.github.com>
2025-08-26 23:54:14 +0900

    ✨EnemyがPlayerPlaneを視野に入れる判定を書こうと試行錯誤中

--- Source/Enemy.cpp
+++ Source/Enemy.cpp
@@ -1,6 +1,8 @@
 #include "Enemy.h"
 #include "../mtgb/DirectX11Draw.h"
 
+Transform* Enemy::pPlayerPlaneTransform_{ nullptr };
+
 Enemy::Enemy(const Vector3& _position) : GameObject(GameObjectBuilder()
 	.SetName("Enemy")
 	.SetPosition(_position)
@@ -10,7 +12,9 @@ Enemy::Enemy(const Vector3& _position) : GameObject(GameObjectBuilder()
 	pTransform_{ Component<Transform>() },
 	hp_{ 100 },
 	radius_{ 30 },
-	speed_{ 5.0f }
+	speed_{ 5.0f },
+	lockOnAngle_{45.0f},
+	lockOnDistance_{30.0f}
 {
 	//hModel_ = OBJ::Load("Model/OBJ/enemy.obj");
 	hModel_ = Fbx::Load("Model/AirPlene.fbx");
@@ -21,6 +25,8 @@ Enemy::Enemy(const Vector3& _position) : GameObject(GameObjectBuilder()
 	massert(hModelCollider_ >= 0 && "当たり判定モデルの読み込みに失敗");
 
 	pTransform_->position.z = 5.0f;
+	// プレイヤーの機体のTransformを取得
+	pPlayerPlaneTransform_ = FindGameObject("PlayerPlane")->Component<Transform>();
 }
 
 Enemy::~Enemy()
@@ -39,3 +45,25 @@ void Enemy::Draw() const
 	Draw::SetShaderOnce(ShaderType::Debug3D);
 	Draw::FBXModel(hModelCollider_, *pTransform_, 0);
 }
+
+void Enemy::Search()
+{
+	Vector3 forward = pTransform_->Forward();
+	Vector3 toPlayer = pPlayerPlaneTransform_->position - pTransform_->position;
+	float distance = toPlayer.Size();
+
+	// 内積
+	float cosTheta = DirectX::XMVector3Dot(forward, Vector3::Normalize(toPlayer)).m128_f32[0];
+
+	// ロックオンする、視野に入っていると判定する角度のラジアン
+	float lockOnAngleRadian = DirectX::XMConvertToRadians(lockOnAngle_);
+
+	if (cosTheta > lockOnAngleRadian)
+	{
+		LOGIMGUI("Enemy:%lld Lock On Player!!!", entityId_);
+	}
+	
+	
+}
+
+
--- Source/Enemy.h
+++ Source/Enemy.h
@@ -9,12 +9,16 @@ public:
 
 	void Update() override;
 	void Draw() const override;
+	void Search();
 private:
 	int hp_;
 	float speed_;
 	Transform* pTransform_;
+	static Transform* pPlayerPlaneTransform_;
 	//OBJModelHandle hModel_;
 	FBXModelHandle hModel_;
 	FBXModelHandle hModelCollider_;
 	float radius_;
+	float lockOnAngle_;// ロックオン、プレイヤーを発見する角度(degree)
+	float lockOnDistance_;// ロックオン、プレイヤーを発見する距離
 };
