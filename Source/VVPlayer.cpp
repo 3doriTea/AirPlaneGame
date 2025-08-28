b2d0b28d1fa6d7abb303141b7ad54caa95678058
3doriTea <sorasora3program@gmail.com>
2025-08-28 21:40:37 +0900

    ➕✨VVPlayerを追加

---
+++ Source/VVPlayer.cpp
@@ -0,0 +1,13 @@
+#include "VVPlayer.h"
+
+VVPlayer::VVPlayer()
+{
+}
+
+VVPlayer::~VVPlayer()
+{
+}
+
+void VVPlayer::Play(const std::string& _text)
+{
+}
---
+++ Source/VVPlayer.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <mtgb.h>
+#include <string>
+#include <queue>
+#include "MTNet/HttpClient.h"
+
+/// <summary>
+/// VOICEVOXを再生するためのクラス
+/// </summary>
+class VVPlayer
+{
+public:
+	VVPlayer();
+	~VVPlayer();
+
+	/// <summary>
+	/// 指定したテキストを読み上げる
+	/// </summary>
+	/// <param name="_text">読み上げるテキスト</param>
+	void Play(const std::string& _text);
+
+private:
+	std::queue<std::string> playList_;  // 台詞の待機キュー
+
+	mtnet::HttpClient httpClient_;  // VOICEVOXのAPI利用用 http通信クライアント
+};
