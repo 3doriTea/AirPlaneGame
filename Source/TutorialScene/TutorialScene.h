d4dcaa6f784671164cf17341868336852ba1b19a
3doriTea <sorasora3program@gmail.com>
2025-08-30 17:14:54 +0900

    ✨チュートリアル作成中

--- Source/TutorialScene/TutorialScene.h
+++ Source/TutorialScene/TutorialScene.h
@@ -1,8 +1,15 @@
 #pragma once
 #include <mtgb.h>
+#include "MTStat/MTStat.h"
 
 class TutorialScene : public mtgb::GameScene
 {
+public:
+	enum STATE
+	{
+		S_
+	};
+
 public:
 	TutorialScene();
 	~TutorialScene();
@@ -12,6 +19,9 @@ public:
 	void Draw() const override;
 	void End() override;
 
+	const STATE GetState() const;
+
 private:
+	STATE currentState_;  // 現在のチュートリアルステート
 	TimerHandle hToPlaySceneTimer_;  // プレイシーン遷移までのタイマー
 };
