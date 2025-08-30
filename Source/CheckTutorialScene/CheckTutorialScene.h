bf9bf477916f837c922b1b8f91428d02a3aea1c2
3doriTea <sorasora3program@gmail.com>
2025-08-30 19:25:33 +0900

    ✨➕背景画像追加

--- Source/CheckTutorialScene/CheckTutorialScene.h
+++ Source/CheckTutorialScene/CheckTutorialScene.h
@@ -3,6 +3,15 @@
 
 class CheckTutorialScene : public mtgb::GameScene
 {
+private:
+	enum IMAGE_LAYER
+	{
+		IL_BACKGROUND,  // 背景
+		IL_PLANE,       // 飛行機
+		IL_MOUNTAIN,    // 手前の山
+		IL_MAX,  // 最大値
+	};
+
 public:
 	CheckTutorialScene();
 	~CheckTutorialScene();
@@ -15,5 +24,8 @@ public:
 private:
 	TimerHandle hToNextSceneTimer_;  // プレイシーン遷移までのタイマー
 	bool isSkipTutorial_;  // チュートリアルをスキップするか
+	ImageHandle hImage_[IL_MAX];
 	ImageHandle hBackground_;  // 背景画像
+	ImageHandle hPlane_;  // 背景飛行機画像
+	ImageHandle hMountain_;  // 背景山画像
 };
