4acfd4ea7abf0870db707373096680453b6abc59
3doriTea <sorasora3program@gmail.com>
2025-08-31 13:09:16 +0900

    ✨チェックチュートリアルシーン一旦完成

--- Source/TutorialScene/TutorialScene.h
+++ Source/TutorialScene/TutorialScene.h
@@ -7,6 +7,13 @@ class TutorialScene : public mtgb::GameScene
 public:
 	enum STATE
 	{
+		S_HANDSHAKE,  // 挨拶
+		S_SHOW_TIMER,  // 時間内に敵を
+		S_SHOW_NOLMA,  // ノルマを
+		S_LEFT_SIDE,  // 左の君は
+		S_RIGHT_SIDE,  // 右の君は
+		S_STICK,  // スティックを倒して
+		S_TRIGGER,  // トリガーを押すと
 		S_
 	};
 
