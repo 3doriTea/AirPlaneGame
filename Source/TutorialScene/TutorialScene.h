2d856885bc590adcf565901a7bfa7c5d5b704940
3doriTea <sorasora3program@gmail.com>
2025-09-03 09:53:42 +0900

    ✨ステートに番号割り振り

--- Source/TutorialScene/TutorialScene.h
+++ Source/TutorialScene/TutorialScene.h
@@ -10,14 +10,14 @@ class TutorialScene : public mtgb::GameScene
 public:
 	enum STATE
 	{
-		S_HANDSHAKE,  // 挨拶
-		S_SHOW_TIMER,  // 時間内に敵を
-		S_SHOW_NOLMA,  // ノルマを
-		S_LEFT_SIDE,  // 左の君は
-		S_RIGHT_SIDE,  // 右の君は
-		S_STICK,  // スティックを倒して
-		S_TRIGGER,  // トリガーを押すと
-		S_
+		S_HANDSHAKE = 0,  // 挨拶
+		S_SHOW_TIMER_NOLMA = 4,  // 時間内にノルマを
+		S_LEFT_SIDE = 5,  // 左の君は
+		S_RIGHT_SIDE = 6,  // 右の君は
+		S_STICK = 7,  // スティックを倒して
+		S_TRIGGER = 8,  // トリガーを押すと
+		S_SLIDER = 9,  // 左の運転手スライダ
+		S_GL = 10,  // 幸運を祈る
 	};
 
 public:
