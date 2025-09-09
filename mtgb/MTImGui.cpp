639549fa65f7a29755a6731e4fc28f7f5f17ca78
西川和樹 <240050@jc-21.jp>
2025-09-09 13:42:48 +0900

    ✨SettingsウィンドウのInputの項目の表示内容を修正

--- mtgb/MTImGui.cpp
+++ mtgb/MTImGui.cpp
@@ -56,17 +56,23 @@ void mtgb::MTImGui::Update()
 
 	DirectShow([]()
 		{
-			Game::System<SceneSystem>().RegisterPendingCallback([]()
-				{
-					if (ImGui::Button("EnumJoystick"))
-					{
-						Game::System<Input>().EnumJoystick();
-					}
-					if (ImGui::Button("SwapInput"))
-					{
-						Game::System<WinCtxResManager>().SwapResource<InputResource>();
-					}
-				});
+            if (ImGui::Button("EnumJoystick"))
+            {
+                Game::System<SceneSystem>().RegisterPendingCallback([]()
+                    {
+                        Game::System<Input>().EnumJoystick();
+
+                    });
+            }
+            if (ImGui::Button("SwapInput"))
+            {
+                Game::System<SceneSystem>().RegisterPendingCallback([]()
+                    {
+                        Game::System<WinCtxResManager>().SwapResource<InputResource>();
+                    });
+
+            }
+			
 
 		}, "Input", ShowType::Settings);
 }
