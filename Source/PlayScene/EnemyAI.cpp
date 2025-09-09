074e5ca7d343d2c04f9e1d0e082332465820039e
3doriTea <sorasora3program@gmail.com>
2025-09-09 10:07:39 +0900

    ✨敵AI書き込み中

--- Source/PlayScene/EnemyAI.cpp
+++ Source/PlayScene/EnemyAI.cpp
@@ -1,14 +1,74 @@
 #include "EnemyAI.h"
 
+namespace
+{
+	// アクティブ範囲距離
+	const float SLEEP_DISTANCE{ 100 };
+	// 回避行動をとる距離
+	const float AVOID_DISTANCE{ 20 };
+}
+
 EnemyAI::EnemyAI()
 {
+	sMain_
+		.OnStart(S_SLEEP,  [this]
+		{
+			out_.isActive = false;  // スリープ開始で非アクティブ化
+		})
+		.OnUpdate(S_SLEEP, [this]
+		{
+			// プレイヤーとの距離がアクティブ範囲内なら、索敵行動に遷移
+			if (GetToPlayerDistance() <= SLEEP_DISTANCE)
+			{
+				sMain_.Change(S_SEARCH);
+			}
+		})
+		.OnEnd(S_SLEEP, [this]
+		{
+			out_.isActive = true;  // スリープ終了でアクティブ化
+		})
+		.OnUpdate(S_SEARCH, [this]
+		{
+			{ /* TODO:索敵行動 */ }
+
+			if (IsForwardToPlayerDir())
+			{
+				sMain_.Change(S_FIGHT);
+			}
+		})
+		.OnUpdate(S_FIGHT, [this]
+		{
+			// 最初のStartを呼び出すために変更
+			sFight_.Change(SF_LOOK_AT_PLAYER);
+		})
+		.OnUpdate(S_FIGHT,  [this]
+		{
+			// 戦闘ステートに任せる
+			sFight_.Update();
+		});
+
+	sFight_
+		.OnUpdate(SF_LOOK_AT_PLAYER, [this]
+		{
+			if ()
+		});
+
+	// 最初のStartを呼び出すために変更する
+	sMain_.Change(S_SLEEP);
 }
 
-void EnemyAI::Update()
+const float EnemyAI::GetToPlayerDistance() const
 {
+	return (input_.playerPos - input_.pSelfTrans->GetWorldPosition()).Size();
 }
 
-void EnemyAI::SetInput(InputData&& _data)
+const bool EnemyAI::IsForwardToPlayerDir() const
 {
-	inputData_ = std::move(_data);
+	Vector3 vRight{ input_.pSelfTrans->Right() };
+	Vector3 vDir{ input_.playerPos - input_.pSelfTrans->GetWorldPosition() };
+
+	// プレイヤーが前方向に居るなら、戦闘行動に遷移
+	float angle{ DirectX::XMVectorGetX(DirectX::XMVector3Dot(vRight, vDir)) };
+	// cosが0未満なら前方向
+	return angle < 0;
 }
--- Source/PlayScene/EnemyAI.h
+++ Source/PlayScene/EnemyAI.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <mtgb.h>
 #include <functional>
 #include "MTStat/MTStat.h"
 
@@ -13,7 +14,17 @@ public:
 	/// </summary>
 	struct InputData
 	{
-
+		Vector3 playerPos;
+		Transform* pSelfTrans{ nullptr };
+	};
+	/// <summary>
+	/// AIからの出力データ
+	/// </summary>
+	struct OutData
+	{
+		bool isActive;
+		bool isFire;
+		Vector3 lookPosition;
 	};
 
 	/// <summary>
@@ -21,9 +32,18 @@ public:
 	/// </summary>
 	enum MAIN_STATE
 	{
-		S_SLEEP,  // 範囲外待機
-		S_FREE,   // 自由行動
-		S_FIGHT,  // 戦闘中
+		S_SLEEP,   // 範囲外待機
+		S_SEARCH,  // 索敵行動
+		S_FIGHT,   // 戦闘行動
+	};
+	/// <summary>
+	/// 戦闘行動ステート
+	/// </summary>
+	enum FIGHT_STATE
+	{
+		SF_LOOK_AT_PLAYER,  // プレイヤーに向ける
+		SF_AVOID,           // プレイヤーに衝突しないように避ける
+		SF_ROUND,           // Z軸でぐるぐる回る
 	};
 
 public:
@@ -33,15 +53,35 @@ public:
 	/// <summary>
 	/// AIの更新処理をする
 	/// </summary>
-	void Update();
-
+	inline void Update() { sMain_.Update(); }
 	/// <summary>
 	/// 入力データをセットする
 	/// </summary>
 	/// <param name="_data">AIの更新時に参照されるデータ</param>
-	static void SetInput(InputData&& _data);
-private:
-	mtstat::MTStat<MAIN_STATE> state_;  // 状態
+	inline void SetInputData(InputData&& _data) { input_ = std::move(_data); }
+	/// <summary>
+	/// 出力データをゲットする
+	/// </summary>
+	/// <returns>出力データの参照</returns>
+	inline const OutData& GetOutData() const { return out_; }
 
-	static InputData inputData_;  // 入力データ
+private:  // 内部で使う便利系メンバ関数
+	/// <summary>
+	/// プレイヤーまでの距離を取得
+	/// </summary>
+	/// <returns>プレイヤーまでの距離</returns>
+	const float GetToPlayerDistance() const;
+
+	/// <summary>
+	/// プレイヤーが前方向にいるか
+	/// </summary>
+	/// <returns>前方向にいる true / false</returns>
+	const bool IsForwardToPlayerDir() const;
+
+private:
+	mtstat::MTStat<MAIN_STATE> sMain_;    // 主な状態
+	mtstat::MTStat<FIGHT_STATE> sFight_;  // 戦闘時の状態
+
+	InputData input_;  // 入力データ
+	OutData out_;      // 出力データ
 };
