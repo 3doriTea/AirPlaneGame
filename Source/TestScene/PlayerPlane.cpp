140e0aa5994594df3b17f215b849c3db87cfb62a
3doriTea <sorasora3program@gmail.com>
2025-09-25 17:19:41 +0900

    ✨プレイヤー操作を滑らかなコントロール

--- Source/TestScene/PlayerPlane.cpp
+++ Source/TestScene/PlayerPlane.cpp
@@ -8,6 +8,8 @@
 #include <cmath>
 using namespace mtgb;
 
+#define USE_SMOOTH_CON 1
+
 namespace
 {
 	float defaultSpeed = 3.0f;
@@ -104,6 +106,31 @@ void PlayerPlane::Update()
 		// WindowContextを直接指定しない方いい
 		Vector2F axis = InputUtil::GetAxis(WindowContext::First);
 
+		LOGF("axis(%f, %f)\n", axis.x, axis.y);
+
+#if USE_SMOOTH_CON
+#pragma region 回転方法0 (細かく)
+		// 上
+		if (axis.y > 0)
+		{
+			curr *= XMQuaternionRotationAxis(pTransform_->Right(), ROT_ANGLE * axis.y);
+		}
+		else if (axis.y < 0)
+		{
+			curr *= XMQuaternionRotationAxis(pTransform_->Right(), ROT_ANGLE * axis.y);
+		}
+		// 右
+		if (axis.x > 0)
+		{
+			curr *= XMQuaternionRotationAxis(pTransform_->Up(), ROT_ANGLE * axis.x);
+		}
+		else if (axis.x < 0)
+		{
+			curr *= XMQuaternionRotationAxis(pTransform_->Up(), ROT_ANGLE * axis.x);
+		}
+#pragma endregion
+#else
+#pragma region 回転方法1 (のっぺり)
 		// 上
 		if (axis.y > 0)
 		{
@@ -122,7 +149,8 @@ void PlayerPlane::Update()
 		{
 			curr *= XMQuaternionRotationAxis(pTransform_->Up(), -ROT_ANGLE);
 		}
-
+#pragma endregion
+#endif
 		// 前方向、頭は上方向に
 		Vector3 forward{ pTransform_->Forward() };
 		curr = Quaternion::SLerp(curr, Quaternion::LookRotation(forward, Vector3::Up()), Time::DeltaTimeF() * HEAD_UP_RATE);
