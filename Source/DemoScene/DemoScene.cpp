8b52c857f90833223496ea96e425aeb044e558e7
guest1 <guest1@PORTABLE_COM>
2025-09-27 16:10:13 +0900

    ✨️ランキングをデモシーンでも表示

--- Source/DemoScene/DemoScene.cpp
+++ Source/DemoScene/DemoScene.cpp
@@ -11,7 +11,22 @@ using namespace mtgb;
 
 namespace
 {
+	static const size_t BUFFER_SIZE{ 1024 };
+	mtbin::Byte buffer[BUFFER_SIZE]{};
 	const float TO_MOVE_TIME_SEC{ 3.0f };
+	const int FULLSCORE_COUNT{ 6 };
+	// Draw用の前回のスコアの表示のための定数
+	const int PREVPAIRSCORE_INDEX{ 5 };
+	const UIParams UI_PARAMS{.depth = 7};
+
+	const int SCORE_POS_X_FIRST{ 110 };
+	const int SCORE_POS_X_SECOND{ 1430 };
+	const int SCORE_POS_Y{ 260 };
+	const int SCORE_MARGIN{ 60 };
+	const int PREPAIR_SCORE_Y{ 140 };
+	const std::string PREPARE_TEXT{ "前回のペアのスコア: " };
+	const int SCORE_SIZE{ 36 };
+	const int PREPARE_SCORE_SIZE{ 36 };
 }
 
 DemoScene::DemoScene() :
@@ -33,6 +48,7 @@ void DemoScene::Initialize()
 
 	Audio::Clear();
 
+	InitializeRanking();
 	Instantiate<SkySphere>();
 
 	// 右側の運転手
@@ -78,6 +94,32 @@ void DemoScene::Update()
 
 void DemoScene::Draw() const
 {
+	if (!rankingList_.empty())
+	{
+		WindowContext ctx = CurrContext();
+		int x = 0;
+		if (ctx == WindowContext::First)
+		{
+			x = SCORE_POS_X_SECOND;
+		}
+		else if (ctx == WindowContext::Second)
+		{
+			x = SCORE_POS_X_FIRST;
+		}
+		Vector2F scorePos = GenDrawScreenFrom(Vector2F{ x,SCORE_POS_Y });
+		int scoreMargin = GenDrawScreenFontSize(SCORE_MARGIN);
+		int scoreSize = GenDrawScreenFontSize(PREPARE_SCORE_SIZE);
+		for (auto i = 0; i < rankingList_.size() - 1; ++i)
+		{
+			Draw::ImmediateText(std::to_string(i + 1) + "位: " + std::to_string(rankingList_[i]),
+				{ scorePos.x, scorePos.y + i * scoreMargin }, SCORE_SIZE, TextAlignment::topLeft, UI_PARAMS);
+		}
+
+		Draw::ImmediateText(PREPARE_TEXT + std::to_string(rankingList_[PREVPAIRSCORE_INDEX]), { scorePos.x, PREPAIR_SCORE_Y }, scoreSize, TextAlignment::topLeft, UI_PARAMS);
+		//Draw::ImmediateText(std::to_string(rankingList_[PREVPAIRSCORE_INDEX]), { static_cast<float>(PREPARE_TEXT.size() * scoreSize), PREPAIR_SCORE_Y}, PREPARE_SCORE_SIZE, TextAlignment::topLeft, UI_PARAMS);
+	}
+	
+
 }
 
 void DemoScene::End()
@@ -86,5 +128,26 @@ void DemoScene::End()
 
 void DemoScene::InitializeRanking()
 {
+	ranking_ = new Ranking();
+	// falseの場合はランキング更新せず、ファイルの有無だけ確認
+	struct _stat s;
+	mtbin::MemoryStream ms{ buffer, BUFFER_SIZE };
+	int rc = _stat("ranking.dat", &s);
+	if (rc == -1)
+	{
+		std::vector<int> initData(FULLSCORE_COUNT, 0);
+		rankingList_ = initData; // ←ここで0埋めを反映
+	}
+	else
+	{
+		// ファイルあれば読み込んでサイズ確保
+		ranking_->LoadFileToMemoryStream("ranking.dat", ms);
+		rankingList_ = ranking_->GetRankingList();
+		if (rankingList_.size() < FULLSCORE_COUNT)
+		{
+			rankingList_.resize(FULLSCORE_COUNT, 0);
+		}
+	}
+	prevPairScore_ = 0; // またはファイル内スコアの初期化処理
 
 }
