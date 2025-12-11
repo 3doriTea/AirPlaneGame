18b8a730b69f50531397ff719d50e6a582f96f85
3doriTea <sorasora3program@gmail.com>
2025-12-12 08:39:26 +0900

    ✨オートパイロットで高さ制限を付けた

--- Source/PlayScene/AutoPilotPlay.cpp
+++ Source/PlayScene/AutoPilotPlay.cpp
@@ -2,10 +2,16 @@
 
 namespace
 {
-	// 作戦範囲外 y軸
-	const float DEAD_ZONE_Y{ 20.0f };
-	// 範囲外から出たときの向かうべき場所
-	const float RETURN_SAFE_Y{ 25.0f };
+	// 作戦範囲外 y軸最低点
+	const float DEAD_ZONE_Y_MIN{ 20.0f };
+	// 作戦範囲外 y軸最高点
+	const float DEAD_ZONE_Y_MAX{ 300.0f };
+
+	// 範囲外から出たときの向かうべき場所 y軸最低点
+	const float RETURN_SAFE_Y_MIN{ 25.0f };
+	// 範囲外から出たときに向かうべき場所 y軸最高点
+	const float RETURN_SAFE_Y_MAX{ 295.0f };
+
 	const float AUTO_ROTATE_ZONE_Y{ 25.0f };
 	const float LOOK_AT_DISTANCE{ 20.0f };
 
@@ -24,10 +30,20 @@ bool AutoPilotPlay::TryUpdate()
 		pTransform_->position.y = 10.0f;
 	}
 
-	if (pos.y <= DEAD_ZONE_Y)
+	// 最低点を超えたときの帰還処理
+	if (pos.y <= DEAD_ZONE_Y_MIN)
 	{
 		Vector3 lookAt{ pTransform_->Forward() * LOOK_AT_DISTANCE + pos };
-		lookAt.y = RETURN_SAFE_Y;
+		lookAt.y = RETURN_SAFE_Y_MIN;
+		pTransform_->rotate = Quaternion::SLerp(pTransform_->rotate, Quaternion::LookRotation(lookAt - pos, Vector3::Up()), LARP_RATE);
+		return true;
+	}
+
+	// 最高点を超えたときの帰還処理
+	if (pos.y >= DEAD_ZONE_Y_MAX)
+	{
+		Vector3 lookAt{ pTransform_->Forward() * LOOK_AT_DISTANCE + pos };
+		lookAt.y = RETURN_SAFE_Y_MAX;
 		pTransform_->rotate = Quaternion::SLerp(pTransform_->rotate, Quaternion::LookRotation(lookAt - pos, Vector3::Up()), LARP_RATE);
 		return true;
 	}
